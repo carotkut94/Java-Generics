6f75a2447cda12a2f97b2421720f5da78632346f
DeathCode <carotkut12@gmail.com>
2018-04-15 18:55:26 +0530

    Generic ArrayList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // If no type provided to the array list, it can hold anything and no error would be thrown
+        // ArrayList items = new ArrayList();
+
+        // but if the item is defined with a type then it can only contain the Integer type of values
+        ArrayList<Integer> items = new ArrayList<>();
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add(5);
+        items.add(6);
+        // In this case ArrayList can hold any sort of data, as no type for the array list is defined yet
+        // So the method printDoubled would throw ClassCastException, as string cannot be casted to Integer
+        // And this error will be thrown at run time only
+        // items.add("Sidhant Rajora");
+
+        //and if the array list is defined with the type, then above line would show error, so commented out.
+        printDoubled(items);
+    }
+
+    private static void printDoubled(ArrayList a){
+        for(Object i : a)
+        {
+            System.out.println((Integer)i*2);
+        }
     }
 }
